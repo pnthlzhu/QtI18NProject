a60996f0d447026b3f5588874107a54b41bcb384
hlzhu <zhu_huiling.pfu@cn.fujitsu.com>
2021-03-28 20:00:34 +0800

    modify I18N

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLocale>
 #include <qtranslator.h>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTranslator translate;
 
-    translate.load("jp.qm");
+    QString name = QLocale::system().name();
+    qDebug() << "locale language is " << name << endl;
+
+    if (name == "ja_JP")
+    {
+        translate.load("jp.qm");
+    }
+    else if (name == "zh_CN")
+    {
+        translate.load("zh.qm");
+    }
+
     a.installTranslator(&translate);
 
     MainWindow w;
